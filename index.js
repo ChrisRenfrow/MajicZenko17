f2214211743e511ce00b48d798e0ff21850b37e6
Justin Chow <jchowchow@gmail.com>
2017-08-16 16:02:21 -0700

    yeah boi

---
+++ index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import ReactDOM, { render } from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import NavBar from './navbar'
+import App from './window'
+
+
+Router.location = '/';
+
+const root = document.querySelector('#root')
+const app = document.querySelector('#app')
+
+// render(<NavBar>
+//   <div style={styles.list}> Heyo </div>
+//   </NavBar>, root)
+render(<App />, app)
